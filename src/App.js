19f4f8050ddccdd311b2e4e5cf96d4c29fce4670
AvkeshSingh <singhavkesh363@gmail.com>
2024-07-30 21:49:31 +0530

    Uploading files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import TicTacToe from '../src/Components/TicTacToe/TicTacToe.jsx';
+//import '../src/Components/TicTacToe/TicTacToe.jsx'
+
+function App() {
+  return (
+    <div>
+       <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
